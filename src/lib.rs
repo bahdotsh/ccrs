77ca765cddd84578fa097e3e21bec2d3bd2b5613
bahdotsh <appu.yess@gmail.com>
2023-05-12 21:56:03 +0530

    added decrypt

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,25 @@ pub fn encrypt(s: &str, shift: u32) -> String {
     cipher_text
 }
 
+pub fn decrypt(s: &str, shift: u32) -> String {
+   let mut cipher_text = String::new(); 
+   for l in s.chars() {
+        cipher_text.push(char::from_u32((l as u32) - shift).unwrap());
+    } 
+
+    cipher_text
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn ecrypt() {
         assert_eq!("dbu", encrypt("cat", 1));
+    }
+
+    #[test]
+    fn encrypt_decrypt() {
+        assert_eq!("cat", decrypt("dbu", 1))
     }
 }
